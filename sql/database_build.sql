6b2c4a2cc601f83df27a550e7a2bea693179cad9
Richard Ostler <richard.ostler@rothamsted.ac.uk>
2025-01-17 17:35:46 +0000

    Update database_build.sql

--- sql/database_build.sql
+++ sql/database_build.sql
@@ -1,46 +1,103 @@
-drop table forecast_data.forecast_model_values;
-drop table forecast_data.forecasts;
-drop table forecast_data.model_runs;
+DROP TABLE forecast_data.day10_forecast_model_values CASCADE;
 
-create table forecast_data.model_runs (
-    id serial not null,
-    node_number int not null,
-    model_run_date date not null,
-    model_type int not null,
-    model_run_day_metadata_actual text null,
-    model_run_day_inferred text not null,
-    forecast_site text not null,
-    source_file text not null,
-    constraint model_runs_pk primary key(id) 
-); 
+DROP TABLE forecast_data.day10_forecasts CASCADE;
 
-comment on column forecast_data.model_runs.model_run_date is 'The metadata last updated value';
-comment on column forecast_data.model_runs.model_run_day_metadata_actual is 'The metadata Model value. In some cases this is blank';
-comment on column forecast_data.model_runs.model_run_day_inferred is 'The metadata Model value inferred from the metadata last updated value. This may be different to the actual day value.';
+DROP TABLE forecast_data.day10_forecast_variable_summaries CASCADE;
 
-create table forecast_data.forecasts (
-    id serial not null,
-    model_run_id integer not null,
-    forecast_date date not null,
-    forecast_day text not null,
-    day_period text not null,
-    forecast_hour text not null,
-    to_include integer null,
-    constraint forecasts_pk primary key(id),
-    constraint forecasts_model_run_id_fk foreign key (model_run_id) references model_runs(id)
+DROP TABLE forecast_data.day10_model_runs CASCADE;
+
+DROP TABLE forecast_data.day28_forecast_model_values CASCADE;
+
+DROP TABLE forecast_data.day28_forecasts CASCADE;
+
+DROP TABLE forecast_data.day28_forecast_variable_summaries CASCADE;
+
+DROP TABLE forecast_data.day28_model_runs CASCADE;
+
+CREATE TABLE day10_model_runs (
+	id serial4 NOT NULL,
+	node_number smallint NULL,
+	last_updated timestamp NOT NULL,
+	model_run date NOT NULL,
+	forecast_site text NOT NULL,
+	source_file text NOT NULL,
+	CONSTRAINT pk_day10_model_runs PRIMARY KEY (id)
 );
 
-create table forecast_data.forecast_model_values (
-	id serial not null,
-	forecast_id int not null,
+CREATE TABLE day10_forecasts (
+	id serial4 NOT NULL,
+	model_run_id smallint NOT NULL,
+	forecast_date date NOT NULL,
+	forecast_day smallint NOT NULL,
+	forecast_hour smallint NOT NULL,
+	CONSTRAINT pk_day10_forecasts PRIMARY KEY (id),
+	CONSTRAINT day10_forecasts_model_run_id_fk FOREIGN KEY (model_run_id) REFERENCES day10_model_runs(id)
+);
+
+CREATE TABLE day10_forecast_variable_summaries (
+	id serial4 NOT NULL,
+	forecast_id int4 NOT NULL,
 	variable text not null,
-	value numeric null,
-	model integer not null,
-	constraint forecast_model_values_pk primary key(id),
-    constraint forecast_model_values_forecasts_id_fk foreign key (forecast_id) references forecasts(id)
+	mean_value numeric null,
+	max_value numeric null,
+	min_value numeric null,
+	nof_members smallint null,
+	units text null,
+	orig_member_value text null,
+	CONSTRAINT pk_day10_forecast_variable_summaries PRIMARY KEY (id),
+	CONSTRAINT day10_forecasts_forecast_id_fk FOREIGN KEY (forecast_id) REFERENCES day10_forecasts(id)
 );
 
+CREATE TABLE day10_forecast_model_values (
+	id serial4 NOT NULL,
+	forecast_id int4 NOT NULL,
+	variable text NOT NULL,
+	value numeric NULL,
+	model_number smallint NOT NULL,
+	CONSTRAINT pk_day10_forecast_model_values PRIMARY KEY (id),
+	CONSTRAINT day10_forecast_model_values_forecasts_id_fk FOREIGN KEY (forecast_id) REFERENCES day10_forecasts(id)
+);
 
+CREATE TABLE day28_model_runs (
+	id serial4 NOT NULL,
+	node_number smallint NULL,
+	last_updated timestamp NOT NULL,
+	model text NULL,
+	forecast_site text NOT NULL,
+	source_file text NOT NULL,
+	CONSTRAINT pk_day28_model_runs PRIMARY KEY (id)
+);
 
+CREATE TABLE day28_forecasts (
+	id serial4 NOT NULL,
+	model_run_id int4 NOT NULL,
+	forecast_date date NOT NULL,
+	forecast_day smallint NOT NULL,
+	forecast_hour smallint NOT NULL,
+	CONSTRAINT pk_day28_forecasts PRIMARY KEY (id),
+	CONSTRAINT day28_forecasts_model_run_id_fk FOREIGN KEY (model_run_id) REFERENCES day28_model_runs(id)
+);
 
---select id from forecast_data.model_runs order by id desc limit 1;
+CREATE TABLE day28_forecast_variable_summaries (
+	id serial4 NOT NULL,
+	forecast_id int4 NOT NULL,
+	variable text not null,
+	mean_value numeric null,
+	max_value numeric null,
+	min_value numeric null,
+	nof_members smallint null,
+	units text null,
+	orig_member_value text null,
+	CONSTRAINT pk_day28_forecast_variable_summaries PRIMARY KEY (id),
+	CONSTRAINT day28_forecasts_forecast_id_fk FOREIGN KEY (forecast_id) REFERENCES day28_forecasts(id)
+);
+
+CREATE TABLE day28_forecast_model_values (
+	id serial4 NOT NULL,
+	forecast_id int4 NOT NULL,
+	variable text NOT NULL,
+	value numeric NULL,
+	model_number smallint NOT NULL,
+	CONSTRAINT pk_day28_forecast_model_values PRIMARY KEY (id),
+	CONSTRAINT day28_forecast_model_values_forecasts_id_fk FOREIGN KEY (forecast_id) REFERENCES day28_forecasts(id)
+);
